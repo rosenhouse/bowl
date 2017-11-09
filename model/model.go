359165b7838d2e0fecdf14c866e8b1ed341dee2e
Gabriel Rosenhouse <grosenhouse@pivotal.io>
2017-11-08 22:03:11 -0800

    model: frame PinsDownTotal

--- model/model.go
+++ model/model.go
@@ -5,18 +5,45 @@ type Throw int
 const ThrowStrike = Throw(0xAAAA)
 const ThrowSpare = Throw(0xBBBB)
 
+func (t Throw) PinsDown() int {
+	if t == ThrowSpare {
+		panic("who knows!")
+	}
+	if t == ThrowStrike {
+		return 10
+	}
+	return int(t)
+}
+
 type Frame []Throw
 
 func (f Frame) PinsDownInFirstThrow() int {
 	if len(f) < 1 {
 		return -1
 	}
-	if f[0] == ThrowStrike {
-		return 10
-	}
-	return int(f[0])
+	return f[0].PinsDown()
 }
 
 func (f Frame) PinsDownTotal() int {
-	panic("not implemented")
+	switch len(f) {
+	case 0:
+		return -1
+	case 1:
+		if f[0] == ThrowStrike {
+			return 10
+		}
+		return -1
+	case 2:
+		if f[1] == ThrowSpare {
+			return 10
+		}
+		return int(f[0]) + int(f[1])
+	case 3: // 10th frame
+		if f[1] == ThrowSpare {
+			return 10 + f[2].PinsDown()
+		}
+		return 20 + f[2].PinsDown()
+	default:
+		panic("invalid frame")
+	}
 }
--- model/model_test.go
+++ model/model_test.go
@@ -20,4 +20,23 @@ var _ = Describe("Frames", func() {
 		Entry("not bad", []Throw{8, 1}, 8),
 		Entry("strike", []Throw{ThrowStrike}, 10),
 	)
+
+	DescribeTable("PinsDownTotal",
+		func(frame Frame, expected int) {
+			Expect(frame.PinsDownTotal()).To(Equal(expected))
+		},
+		Entry("frame not started", []Throw{}, -1),
+		Entry("incomplete frame", []Throw{8}, -1),
+		Entry("zeros", []Throw{0, 0}, 0),
+		Entry("bagel disabled", []Throw{0, 1}, 1),
+		Entry("not bad", []Throw{8, 1}, 9),
+		Entry("spare", []Throw{4, ThrowSpare}, 10),
+		Entry("strike", []Throw{ThrowStrike}, 10),
+		Entry("10th frame: max score", []Throw{ThrowStrike, ThrowStrike, ThrowStrike}, 30),
+		Entry("10th frame: almost max", []Throw{ThrowStrike, ThrowStrike, 7}, 27),
+		Entry("10th frame: spare strike", []Throw{3, ThrowSpare, ThrowStrike}, 20),
+		Entry("10th frame: spare ok", []Throw{3, ThrowSpare, 2}, 12),
+		Entry("10th frame: spare oops", []Throw{3, ThrowSpare, 0}, 10),
+		Entry("10th frame: meh", []Throw{3, 6}, 9),
+	)
 })
